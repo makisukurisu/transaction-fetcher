3e4c69acdfab91b4695d2cf7494540fea6f999ce
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2025-04-19 11:07:23 +0300

    fix: Add file loggers

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 
 db_logger = logging.getLogger("db")
 
@@ -13,6 +14,21 @@ db_logger.handlers[0].setFormatter(
 )
 db_logger.handlers[0].setLevel(logging.INFO)
 
+db_logger.addHandler(
+    logging.handlers.RotatingFileHandler(
+        "db.log",
+        maxBytes=1024 * 1024 * 10,  # 10 MB
+        backupCount=10,
+    )
+)
+db_logger.handlers[1].setFormatter(
+    logging.Formatter(
+        fmt="[%(asctime)s] %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+)
+db_logger.handlers[1].setLevel(logging.WARNING)
+
 
 main_logger = logging.getLogger("main")
 main_logger.setLevel(logging.INFO)
@@ -25,3 +41,17 @@ main_logger.handlers[0].setFormatter(
     )
 )
 main_logger.handlers[0].setLevel(logging.INFO)
+main_logger.addHandler(
+    logging.handlers.RotatingFileHandler(
+        "main.log",
+        maxBytes=1024 * 1024 * 10,  # 10 MB
+        backupCount=10,
+    )
+)
+main_logger.handlers[1].setFormatter(
+    logging.Formatter(
+        fmt="[%(asctime)s] %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+)
+main_logger.handlers[1].setLevel(logging.WARNING)
